cc0fd4e5e0d72fa63ce7d14095edc2a2b4e1f54b
BAYMAX\Felix <felix@playgen.com>
2017-05-12 15:26:06 +0100

    Add Day/Night Cycle

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float RotateXSpeed;
+    public float RotateYSpeed;
+    public float RotateZSpeed;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(RotateXSpeed, RotateYSpeed, RotateZSpeed) * Time.deltaTime);
+    }
+}
