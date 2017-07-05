8f139c80c12446a852ef70190da6ae112e513bbb
BAYMAX\Felix <felix@playgen.com>
2017-07-05 10:21:18 +0100

    add port number to main menu screen

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
@@ -13,26 +14,31 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private MenuManager _menuManager;
 
     public InputField IpAddress;
+    public InputField Port;
 
     void Awake()
     {
         if (_useDefault)
         {
             IpAddress.text = _networkManager.networkAddress;
+            Port.text = _networkManager.networkPort.ToString();
         }
     }
 
     public void Connect()
     {
         _networkManager.networkAddress = IpAddress.text;
+        _networkManager.networkPort = Convert.ToInt16(Port.text);
+
         _networkManager.StartClient();
         _menuManager.HideMenu();
-
     }
 
     public void HostServer()
     {
         _networkManager.networkAddress = IpAddress.text;
+        _networkManager.networkPort = Convert.ToInt16(Port.text);
+
         _networkManager.StartServer();
         _menuManager.HideMenu();
     }
