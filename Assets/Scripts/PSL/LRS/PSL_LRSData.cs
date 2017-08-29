7ce1632c938e07f5b4158528b2521cdd3686e9c4
BAYMAX\Felix <felix@playgen.com>
2017-08-29 14:33:01 +0100

    Add preliminary code for creating data in the format for integrating with lrs

---
+++ Assets/Scripts/PSL/LRS/PSL_LRSData.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+public class PSL_LRSData
+{
+    private string _api = "";
+    private string _postExtension = "";
+
+    private string _verbUrl = "http://prosociallearn.eu/plsxapi/verbs/";
+    private string _gameId = "";
+
+    public PSL_LRSFormat Data;
+
+    public PSL_LRSData GetData(string playerId, string gameSituationId, string verb, float value)
+    {
+        var data = new PSL_LRSFormat();
+
+        data.Actor.ObjectType = "Agent";
+        data.Actor.Account.Name = playerId;
+        data.Verb.Id = _verbUrl + verb;
+        data.Result.Score.Raw = value;
+        data.Context.ContextActivities.Parent = new Object[1];
+        data.Context.ContextActivities.Parent[0].Id = _gameId;
+        data.Context.ContextActivities.Parent[0].ObjectType = "Activity";
+        data.Object.Id = gameSituationId;
+        data.Object.ObjectType = "Activity";
+
+        var dataObject = new PSL_LRSData()
+        {
+            Data = data
+        };
+
+        return dataObject;
+    }
+
+    public void SendData(PSL_LRSData data)
+    {
+        var requestApi = _api + _postExtension;
+        var body = JsonConvert.SerializeObject(data.Data);
+
+
+        var www = new WWW(requestApi);
+
+        // TODO send data
+    }
+
+}
