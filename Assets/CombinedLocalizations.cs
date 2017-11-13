32d3962a562ff6f4e3508b2e55b3fee40c8e7e5f
BAYMAX\Felix <felix@playgen.com>
2017-11-13 17:57:54 +0000

    improve layout of how to play so font is all the same size

---
+++ Assets/CombinedLocalizations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayGen.Unity.Utilities.Localization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombinedLocalizations : MonoBehaviour
+{
+
+    public List<string> Keys;
+    public bool SeparateKeys;
+
+    void Start()
+    {
+        var text = GetComponent<Text>();
+        var str = "";
+        for (var i = 0; i < Keys.Count; i++)
+        {
+            str += Localization.Get(Keys[i]);
+            if (i + 1 < Keys.Count && SeparateKeys)
+            {
+                str += Environment.NewLine + Environment.NewLine;
+            }
+        }
+        text.text = str;
+    }
+}
