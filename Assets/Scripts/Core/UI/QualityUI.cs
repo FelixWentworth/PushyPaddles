3cb9fd34cad8e66d89d9fa8b4f7248dc8a11d134
BAYMAX\Felix <felix@playgen.com>
2017-11-14 15:34:38 +0000

    Add quit button

--- Assets/Scripts/Core/UI/QualityUI.cs
+++ Assets/Scripts/Core/UI/QualityUI.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class QualityUI : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _buttonSelectedGameObjects;
-    private GameObject _optionsPanel;
+    [SerializeField] private GameObject _optionsPanel;
+    [SerializeField] private GameObject _inGameBackground;
+    [SerializeField] private GameObject _quitGameObject;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
-	    _optionsPanel = transform.Find("Options").gameObject;
+	    _inGameBackground.SetActive(false);
+	    _quitGameObject.SetActive(false);
 	    _optionsPanel.SetActive(false);
 	}
 
     public void ToggleMenu()
     {
         _optionsPanel.SetActive(!_optionsPanel.activeSelf);
+        _inGameBackground.SetActive(SP_Manager.Instance.Get<SP_GameManager>().GameSetup());
+        _quitGameObject.SetActive(SP_Manager.Instance.Get<SP_GameManager>().GameSetup());
     }
 	
 	// Update is called once per frame
